195f23d4d464fb7ab6f480729a1f5b9733c12df3
Ignacio Arias <1992ignacio@gmail.com>
2020-11-26 17:04:32 -0800

    Initial Commit

---
+++ nrmvcuikitcoredatatesting003nasa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nrmvcuikitcoredatatesting003nasa
+//
+//  Created by Ignacio Arias on 2020-11-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
